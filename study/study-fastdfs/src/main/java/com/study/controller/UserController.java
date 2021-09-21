127fccfb0dddca0230712c1cd388b91d635f2d6a
zhangminzhong <1065639989@qq.com>
2021-09-21 11:11:43 +0800

    fix:study nacos fastdfs

--- study/study-fastdfs/src/main/java/com/study/controller/UserController.java
+++ study/study-fastdfs/src/main/java/com/study/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.study.bean.User;
 import com.study.config.UserConfig;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class UserController {
 
     @GetMapping("/getUser")
     public User getUser(){
+        log.info(JSON.toJSONString(userConfig));
         log.info(userConfig.toString());
         log.info(userConfig.userName);//取不到值
         return new User(userConfig.getUserName(), userConfig.getPassword());
---
+++ study/study-fastdfs/src/test/java/com/study/StudyFastdfsApplicationTest.java
@@ -0,0 +1,30 @@
+package com.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.*;
+
+/**
+ * @author zhang minzhong
+ * @date 2021/9/21 10:02
+ */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class StudyFastdfsApplicationTest {
+
+    public void setUp() throws Exception {
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testUpload() throws Exception {
+        String uploadFilePath="D:\\new.txt";
+        File file = new File(uploadFilePath);
+    }
+}
